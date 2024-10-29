eb95434b0c17e1644dad6ad597476cd1791ca5b0
Thiyagu2009 <thiyagu.nataraj@gmail.com>
2024-10-29 19:00:27 +0530

    Upgraded database to postgres

--- dealflow/settings.py
+++ dealflow/settings.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 
 from pathlib import Path
@@ -93,10 +94,10 @@ WSGI_APPLICATION = 'dealflow.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
